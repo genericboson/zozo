571145739004277eeccd8724717b60be9dd4097c
jeonghunlee <ljhoo8@naver.com>
2025-11-07 22:17:54 +0900

    - server : start servers batch file

---
+++ StartServers.bat
@@ -0,0 +1,12 @@
+@echo off
+
+rem -- If you want to enable ANSI code coloring,
+rem -- Add VirtualTerminalLevel=1 as DWORD to HKEY_CURRENT_USER\Console
+
+path out\build\x64-debug\bin
+
+taskkill /f /im LOBBY_SERVER.exe
+start LOBBY_SERVER.exe
+
+taskkill /f /im GAME_SERVER.exe
+start GAME_SERVER.exe
