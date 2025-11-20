2b2d36c86fae7262184acab9285e3e54625a4a0d
jeonghunlee <ljhoo8@naver.com>
2025-11-20 23:50:02 +0900

    - server : GetServerIds

--- LobbyServer/Session/LobbyStubManager.cpp
+++ LobbyServer/Session/LobbyStubManager.cpp
@@ -7,6 +7,19 @@ namespace GenericBoson
 {
 	void LobbyStubManager::AddLobbyStub(std::shared_ptr<LobbyStub>&& pLobbyStub)
 	{
+		std::unique_lock<std::shared_mutex> lock(m_lock);
+
 		m_lobbyStubs[pLobbyStub->Id()] = pLobbyStub;
+	}
+
+	std::vector<int64_t> LobbyStubManager::GetServerIds()
+	{
+		std::vector<int64_t> ids;
+
+		ids.reserve(m_lobbyStubs.size());
+
+		ids.assign(m_lobbyStubs.begin(), m_lobbyStubs.end());
+
+		return ids;
 	}
 }
--- LobbyServer/Session/LobbyStubManager.h
+++ LobbyServer/Session/LobbyStubManager.h
@@ -10,6 +10,10 @@ namespace GenericBoson
 	{
 	public:
 		void AddLobbyStub(std::shared_ptr< LobbyStub >&& pLobbyStub);
+		std::vector<int64_t> GetServerIds();
+
+	private:
+		std::shared_mutex m_lock;
 
 	private:
 		std::unordered_map< int64_t, std::shared_ptr< LobbyStub > > m_lobbyStubs;
