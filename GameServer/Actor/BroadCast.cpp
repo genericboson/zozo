d98984101b591aead075056bd650b124c834b441
jeonghunlee <ljhoo8@naver.com>
2025-12-18 23:53:45 +0900

    - server : PositionCast::Cast

--- GameServer/Actor/BroadCast.cpp
+++ GameServer/Actor/BroadCast.cpp
@@ -3,14 +3,20 @@
 #include "BroadCast.h"
 #include "Character/Character.h"
 
+#include "MessageSchema/External/CharacterPositionUpdate_generated.h"
+
 namespace GenericBoson
 {
 	void PositionCast::Cast(const std::shared_ptr<Character>& pCharacter)
 	{
 		flatbuffers::FlatBufferBuilder fbb;
 
-		
+		const auto req = Zozo::CreateCharacterPositionUpdateReq(
+			fbb,
+			&position);
 
-		pCharacter->Write();
+		fbb.Finish(req);
+
+		pCharacter->Write(fbb.GetBufferPointer(), fbb.GetSize());
 	}
 }
